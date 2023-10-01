451e3faf218f9e83bcd496980aea944a158b647d
trlla2 <116338442+trlla2@users.noreply.github.com>
2023-10-01 13:30:08 +0200

    ya esta jajaj mu buen juegp

--- Scene/Ball.gd
+++ Scene/Ball.gd
@@ -1,7 +1,6 @@
 extends RigidBody2D
 
-@onready var game_over = preload("res://Scene/GameOver.tscn")
-var gameOver = game_over.instance()	
+
 
 var speed = 300
 var dirX = -1
@@ -30,20 +29,13 @@ func _on_area_2d_body_entered(body):
 	
 	if body.is_in_group("Player") : 
 		dirX = -dirX
-<<<<<<< HEAD
-		
-		get_node("AudioStreamPlayer").play()
-		
-	if body.is_in_group(("left_wall")):
-=======
 		get_node("AudioStreamPlayer").play()	
 	elif body.is_in_group(("left_wall")):
->>>>>>> 1469f163d6c1dacb4374d5c6a3e0bbd5a4d923c1
 		speed += 50
 	elif body.is_in_group("horizontal_Wall"):
 		dirY = -dirY	
 	elif body.is_in_group("Death"): 
-		add_child(gameOver)
+		get_tree().change_scene_to_file("res://Scene/GameOver.tscn")
 		
 	
 #get_viewport().size = Vector2(1920, 1080)
