4df396194eb91bec7c1f65b7c143c2c648ced672
trlla2 <116338442+trlla2@users.noreply.github.com>
2023-10-01 13:05:25 +0200

    Update Ball.gd

--- Scene/Ball.gd
+++ Scene/Ball.gd
@@ -1,14 +1,13 @@
 extends RigidBody2D
 
-# @onready var ball_preload = preload("res://scenes/ball.tscn)
-# var ball = ball_preload.instantiate()
-#
-# add_child(ball)
+@onready var game_over = preload("res://Scene/GameOver.tscn")
+var gameOver = game_over.instance()	
 
 var speed = 300
 var dirX = -1
 var dirY = -1
 var spawnPos = Vector2(576,324)
+
 
 #dibujar la BOla
 func _draw() -> void:
@@ -24,20 +23,21 @@ func _process(delta):
 	linear_velocity.y = speed * dirY
 	#print(dir)
 
+		
+	
+
 func _on_area_2d_body_entered(body):
 	
-	if body.is_in_group("Player") :
+	if body.is_in_group("Player") : 
 		dirX = -dirX
-		get_node("AudioStreamPlayer").play()
-		
-	if body.is_in_group(("left_wall")):
+		get_node("AudioStreamPlayer").play()	
+	elif body.is_in_group(("left_wall")):
 		speed += 50
-	if body.is_in_group("horizontal_Wall"):
+	elif body.is_in_group("horizontal_Wall"):
 		dirY = -dirY	
+	elif body.is_in_group("Death"): 
+		add_child(gameOver)
+		
 	
-	elif body.is_in_group("Death"):
-		print("death")
-		
-		
 #get_viewport().size = Vector2(1920, 1080)
 		
